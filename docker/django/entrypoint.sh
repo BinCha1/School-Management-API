1a4cfe4135613dde91b9403eb4fa9f90cd244a0f
BinCha1 <contactwithbinisha@gmail.com>
2026-05-03 12:57:03 +0545

    initial project setup (django + drf + docker + apps structure)

---
+++ docker/django/entrypoint.sh
@@ -0,0 +1,10 @@
+echo "Waiting for postgres..."
+
+sleep 5
+
+echo "Applying migrations..."
+python manage.py makemigrations
+python manage.py migrate
+
+echo "Starting server..."
+python manage.py runserver 0.0.0.0:8000
