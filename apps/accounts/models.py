ab11b1a14fabd9ef4a19941fcd1f39dfba8cae4d
BinCha1 <contactwithbinisha@gmail.com>
2026-05-03 14:20:11 +0545

    feat:setup custom user model with role-based authentication structure

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+
+    class Role(models.TextChoices):
+        STUDENT = "STUDENT", "Student"
+        TEACHER = "TEACHER", "Teacher"
+        PRINCIPAL = "PRINCIPAL", "Principal"
+
+    role = models.CharField(max_length=20, choices=Role.choices)
+
+    def __str__(self):
+        return f"{self.username} ({self.role})"
