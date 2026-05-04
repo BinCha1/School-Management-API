dfb088834f4bf1b86d7ad0a16e9a302c94868b7b
contactwithbinisha@gmail.com <contactwithbinisha@gmail.com>
2026-05-04 11:56:09 +0545

    feat(academic):complete school domain models and admin validation

--- apps/academics/admin.py
+++ apps/academics/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Subject, SchoolClass, StudentProfile, TeacherProfile
 
-# Register your models here.
+admin.site.register(Subject)
+admin.site.register(SchoolClass)
+admin.site.register(StudentProfile)
+admin.site.register(TeacherProfile)
--- apps/academics/models.py
+++ apps/academics/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from apps.accounts.models import User
 
-# Create your models here.
+
+class Subject(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class TeacherProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=20)
+    qualification = models.CharField(max_length=100)
+    classes = models.ManyToManyField(
+        "SchoolClass", blank=True
+    )  # teacher teach multiple class
+
+    def __str__(self):
+        return self.user.email
+
+
+class SchoolClass(models.Model):
+    name = models.CharField(max_length=100)
+    section = models.CharField(max_length=50)
+    subjects = models.ManyToManyField(Subject)  # one class has many subject
+    class_teacher = models.ForeignKey(
+        TeacherProfile,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="class_teacher_of",
+    )  # one responsible class teacher
+
+    def __str__(self):
+        return f"{self.name} - {self.section}"
+
+
+class StudentProfile(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE
+    )  # one student has many class
+    admission_number = models.CharField(max_length=50)
+    classes = models.ManyToManyField(SchoolClass)
+
+    def __str__(self):
+        return self.user.email
